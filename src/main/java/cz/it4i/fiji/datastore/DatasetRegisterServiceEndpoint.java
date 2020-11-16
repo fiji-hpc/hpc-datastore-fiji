0e15ddaac0860a8ab1b8e76d1be723c3b35e39d5
Jan Kožusznik <jan@kozusznik.cz>
2020-11-16 20:00:18 +0100

    code: Cleaning.

--- src/main/java/cz/it4i/fiji/datastore/DatasetRegisterServiceEndpoint.java
+++ src/main/java/cz/it4i/fiji/datastore/DatasetRegisterServiceEndpoint.java
@@ -60,35 +60,4 @@ public class DatasetRegisterServiceEndpoint {
 			rY + "/" + rZ + "/" + version + "?mode=" + mode)).build();
 	}
 
-	/*	@Route(path = "/dataset/:" + UUID, methods = HttpMethod.GET)
-		public String get(@Param(UUID) String uuid) {
-			return "get uuid: " + uuid;
-		}
-	
-		@Route(path = "/dataset/:" + UUID, methods = HttpMethod.DELETE)
-		public String delete(@Param(UUID) String uuid) {
-			return "delete uuid: " + uuid;
-		}
-	
-		@Route(path = "/dataset/:" + UUID + "/:" + X_PARAM + "/:" + Y_PARAM + "/:" +
-			Z_PARAM, methods = HttpMethod.GET)
-		public String getXYZ(@Param(UUID) String uuid, @Param(X_PARAM) String x,
-			@Param(Y_PARAM) String y, @Param(Z_PARAM) String z)
-		{
-			return "get uuid: " + uuid + " x y z: " + x + " " + y + " " + z + " ";
-		}
-	
-		@Route(path = "/dataset/:" + UUID + "/metadata", methods = HttpMethod.GET)
-		public String getMetadata(@Param(UUID) String uuid)
-		{
-			return "getMetadata uuid: " + uuid;
-		}
-	
-		@Route(path = "/dataset/:" + UUID + "/metadata", methods = { HttpMethod.PUT,
-			HttpMethod.POST })
-		public String setMetadata(@Param(UUID) String uuid) {
-			return "setMetadata uuid: " + uuid;
-		}*/
-
-
 }
