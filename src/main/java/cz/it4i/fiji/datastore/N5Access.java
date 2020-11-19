718d1b75b74a3881cbfa668e25dff5378eb19750
Jan Kožusznik <jan@kozusznik.cz>
2020-11-19 14:37:21 +0100

    Allow set downsampling and block size.

--- src/main/java/cz/it4i/fiji/datastore/N5Access.java
+++ src/main/java/cz/it4i/fiji/datastore/N5Access.java
@@ -26,6 +26,8 @@ import java.util.stream.IntStream;
 import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.list.AbstractListImg;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.real.FloatType;
 
@@ -40,11 +42,14 @@ import bdv.export.ExportScalePyramid.LoopbackHeuristic;
 import bdv.export.ProgressWriter;
 import bdv.export.n5.WriteSequenceToN5;
 import bdv.img.n5.BdvN5Format;
+import bdv.img.n5.N5ImageLoader;
 import lombok.extern.slf4j.Slf4j;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.XmlIoSpimData;
 import mpicbg.spim.data.generic.sequence.ImgLoaderHint;
+import mpicbg.spim.data.registration.ViewRegistration;
+import mpicbg.spim.data.registration.ViewRegistrations;
 import mpicbg.spim.data.sequence.Angle;
 import mpicbg.spim.data.sequence.Channel;
 import mpicbg.spim.data.sequence.ImgLoader;
@@ -60,8 +65,9 @@ import mpicbg.spim.data.sequence.VoxelDimensions;
 public class N5Access {
 
 	public static void main(String[] args) throws IOException, SpimDataException {
-		createNew("/tmp/n5", new int[] { 1000, 1000, 100 }, 100,
-			3, 1000, new RawCompression());
+		createNew("/tmp/n5", new int[] { 1000, 1000, 100 }, 2, 2, 2,
+			new RawCompression(), new int[][] { { 1, 1, 1 }, { 2, 2, 2 } },
+			new int[][] { { 64, 64, 64 }, { 64, 64, 64 } });
 	}
 
 	private Path baseDirectory;
@@ -77,12 +83,14 @@ public class N5Access {
 	}
 
 	public static N5Access createNew(String path, int[] dimensions,
-		int timepoints, int channels, int angles, Compression compression)
+		int timepoints, int channels, int angles, Compression compression,
+		int[][] resolutions, int[][] subdivisions)
 		throws IOException, SpimDataException
 	{
 		N5Access result = new N5Access(path);
-		result.createNew(dimensions, timepoints, channels, angles, compression);
-		result.loadFromXML(path);
+		SpimData data = result.createNew(dimensions, timepoints, channels, angles,
+			compression, resolutions, subdivisions);
+		result.saveToXML(data, path);
 		return result;
 	}
 
@@ -119,24 +127,48 @@ public class N5Access {
 		data = new XmlIoSpimData().load(path);
 	}
 
-	private void createNew(int[] dimensions, int timepoints, int channels,
-		int angles, Compression compression) throws IOException
+	private void saveToXML(SpimData aData, String path) throws SpimDataException {
+		new XmlIoSpimData().save(aData, path);
+		this.data = aData;
+	}
+
+	private SpimData createNew(int[] dimensions, int timepoints, int channels,
+		int angles, Compression compression, int[][] resolutions,
+		int[][] subdivisions) throws IOException
 	{
 
-		final Collection<TimePoint> timepointIds = IntStream.range(0, timepoints)
+		final Collection<TimePoint> timepointsCol = IntStream.range(0, timepoints)
 			.<TimePoint> mapToObj(TimePoint::new).collect(Collectors.toList());
 
 		Collection<ViewSetup> viewSetups = generateViewSetups(dimensions,
 			channels, angles);
 
 		Map<Integer, ExportMipmapInfo> exportMipmapInfo = generateMipmapInfo(
-			viewSetups, dimensions);
+			viewSetups, resolutions, subdivisions);
+		long[] lDimensions = Arrays.stream(dimensions).asLongStream().toArray();
+		SequenceDescription sequenceDescription = new SequenceDescription(new TimePoints(
+			timepointsCol), viewSetups, new ImgLoaderImpl<>(new IntType(),
+					lDimensions));
+		WriteSequenceToN5.writeN5File(sequenceDescription, exportMipmapInfo, compression, baseDirectory.toFile(),
+			new LoopbackHeuristicAdapter(), new AfterEachPlaneAdapter(), 1,
+			new ProgressWriterAdapter());
+		sequenceDescription = new SequenceDescription(new TimePoints(timepointsCol),
+			viewSetups, new N5ImageLoader(baseDirectory.toFile(),
+				sequenceDescription));
+		return new SpimData(baseDirectory.toFile(), sequenceDescription,
+			new ViewRegistrations(generateViewRegistrations(timepointsCol,
+				viewSetups)));
+	}
 
-		WriteSequenceToN5.writeN5File(new SequenceDescription(new TimePoints(
-			timepointIds), viewSetups, new ImgLoaderImpl<>(new IntType())),
-			exportMipmapInfo, compression, baseDirectory
-				.toFile(), new LoopbackHeuristicAdapter(), new AfterEachPlaneAdapter(),
-			1, new ProgressWriterAdapter());
+	private Map<Integer, ExportMipmapInfo> generateMipmapInfo(
+		Collection<ViewSetup> viewSetups, int[][] resolutions, int[][] subdivisions)
+	{
+		ExportMipmapInfo info = new ExportMipmapInfo(resolutions, subdivisions);
+		Map<Integer, ExportMipmapInfo> result = new HashMap<>();
+		for (ViewSetup viewSetup : viewSetups) {
+			result.put(viewSetup.getId(), info);
+		}
+		return result;
 	}
 
 	private Collection<ViewSetup> generateViewSetups(int[] dimensions,
@@ -151,20 +183,20 @@ public class N5Access {
 				ViewSetup vs = new ViewSetup(setupId, "setup" + setupId,
 					new FinalDimensions(dimensions), null, channelObj, angleObj, null);
 				viewSetups.add(vs);
+				setupId++;
 			}
-			setupId++;
 		}
 		return viewSetups;
 	}
 
-	private Map<Integer, ExportMipmapInfo> generateMipmapInfo(
-		Collection<ViewSetup> viewSetups, int[] dimensions)
+	private Collection<ViewRegistration> generateViewRegistrations(
+		Collection<TimePoint> timepoints, Collection<ViewSetup> viewSetups)
 	{
-		ExportMipmapInfo info = new ExportMipmapInfo(new int[][] { dimensions }, new int[][] {
-			{ 1, 1, 1 } });
-		Map<Integer, ExportMipmapInfo> result = new HashMap<>();
+		Collection<ViewRegistration> result = new LinkedList<>();
 		for (ViewSetup viewSetup : viewSetups) {
-			result.put(viewSetup.getId(), info);
+			for (TimePoint timePoint : timepoints) {
+				result.add(new ViewRegistration(timePoint.getId(), viewSetup.getId()));
+			}
 		}
 		return result;
 	}
@@ -287,8 +319,8 @@ public class N5Access {
 
 		private SetupImgLoader<T> loader;
 
-		public ImgLoaderImpl(T type) {
-			loader = new SetupImgLoaderImpl(type);
+		public ImgLoaderImpl(T type, long[] dimensions) {
+			loader = new SetupImgLoaderImpl<>(type, dimensions);
 		}
 
 		@Override
@@ -299,16 +331,18 @@ public class N5Access {
 		private static class SetupImgLoaderImpl<U> implements SetupImgLoader<U> {
 
 			private U type;
+			private RandomAccessibleInterval<U> interval;
 
-			public SetupImgLoaderImpl(U type) {
+			public SetupImgLoaderImpl(U type, long[] dimensions) {
 				this.type = type;
+				this.interval = new EmptyImg<>(type, dimensions);
 			}
 
 			@Override
 			public RandomAccessibleInterval<U> getImage(int timepointId,
 				ImgLoaderHint... hints)
 			{
-				return null;
+				return interval;
 			}
 
 			@Override
@@ -337,5 +371,32 @@ public class N5Access {
 	}
 
 
+	private static class EmptyImg<T> extends AbstractListImg<T> {
+
+		private T type;
+
+		public EmptyImg(T type, long[] size) {
+			super(size);
+			this.type = type;
+		}
+
+		@Override
+		public Img<T> copy() {
+			return this;
+		}
+
+		@Override
+		protected T get(int index) {
+			return type;
+		}
+
+		@Override
+		protected void set(int index, T value) {
+
+		}
+
+
+
+	}
 
 }
