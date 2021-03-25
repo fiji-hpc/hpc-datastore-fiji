ece49b216824d5b3d01ac8316d2ef7f4c814d4fe
Jan Kožusznik <jan@kozusznik.cz>
2021-03-25 10:59:22 +0100

    Added comments.

--- src/main/java/cz/it4i/fiji/datastore/register_service/Dataset.java
+++ src/main/java/cz/it4i/fiji/datastore/register_service/Dataset.java
@@ -24,16 +24,78 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Dataset extends BaseEntity {
 
+ private static final long serialVersionUID = 4257740020931057972L;
+
 	@Getter
 	@Setter
 	private UUID uuid;
 
+	private ResolutionLevel resolutionLevel;
+	
+	// TODO Use Collection instead of array
+	// TODO resolutionLevel
 	@Getter
 	@Setter
 	private String path;
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4257740020931057972L;
-
+		
+	@OneToMany
+	private DatasetVersion[] datasetVersion;
+	
+	@Getter
+	@Setter
+	@OneToMany
+	private ViewSetup[] viewSetup;
+	
+//TODO  voxelType is a org.janelia.saalfeldlab.n5.DataType
+	@Getter
+	@Setter
+	private String voxelType;
+	
+	@Getter
+	@Setter
+	private String UUID;
+	
+	@Getter
+	@Setter
+	private String metadata;
+	
+	@Getter
+	@Setter
+	private long[] dimensions;
+	
+	@Getter
+	@Setter
+	private int timepoints;
+	
+	@Getter
+	@Setter
+	private int channels;
+	
+	@Getter
+	@Setter
+	private int angles;
+	
+	@Getter
+	@Setter
+	private String voxelUnit;
+	
+	@Getter
+	@Setter
+	private double[] voxelResolution;
+	
+	@Getter
+	@Setter
+	private Resolution timepointResolution;
+	
+	@Getter
+	@Setter
+	private Resolution channelResolution;
+	
+	@Getter
+	@Setter
+	private Resolution angleResolution;
+	
+	@Getter
+	@Setter
+	private String compression;
 }
