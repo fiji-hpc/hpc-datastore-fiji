e0e8f7670634b6533abef0c5089d0ca7527897a5
Vladimír Ulman <ulman@fi.muni.cz>
2021-07-27 22:21:10 +0200

    CHG,FIX: ImagePlusTransferrer.writeWithAType() actually does write

--- src/main/java/cz/it4i/fiji/legacy/ImagePlusTransferrer.java
+++ src/main/java/cz/it4i/fiji/legacy/ImagePlusTransferrer.java
@@ -10,6 +10,8 @@ import net.imglib2.type.numeric.RealType;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.NoSuchElementException;
@@ -125,14 +127,26 @@ public class ImagePlusTransferrer extends ImagePlusDialogHandler {
 	public <T extends NativeType<T> & RealType<T>>
 	void writeWithAType(final Dataset inDatasetImg) {
 		try {
-			final Imglib2Types.TypeHandler<T> th = Imglib2Types.getTypeHandler(di.voxelType);
-			final Img<T> img = th.createPlanarImgFactory().create(maxX-minX+1,maxY-minY+1,maxZ-minZ+1);
+			final Imglib2Types.TypeHandler<T> th = Imglib2Types.getTypeHandler(inDatasetImg.getType());
+			final Img<T> img = (Img)inDatasetImg.getImgPlus().getImg();
 
-			final InputStream dataSrc = new URL(requestDatasetServer()).openStream();
+			//sanity check already at the client
+			final Imglib2Types.TypeHandler<T> thServer = Imglib2Types.getTypeHandler(di.voxelType);
+			if (!thServer.nativeAndRealType.equals(th.nativeAndRealType))
+				throw new IllegalArgumentException("Connecting to a server for a type "+thServer.httpType
+						+" with a Dataset of a type "+th.httpType);
+
+			final HttpURLConnection connection = (HttpURLConnection) new URL(requestDatasetServer()).openConnection();
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Content-Type","application/octet-stream"); //to prevent from 415 err code (Unsupported Media Type)
+			connection.setDoOutput(true);
+			connection.connect();
+			final OutputStream dataTgt = connection.getOutputStream();
 
 			//shared buffers to be re-used (to reduce calls to the operator 'new')
-			final byte[] header = new byte[12];
 			byte[] pxData = new byte[0];
+			final byte[] header = new byte[12];
+			final ByteBuffer wrapperOfHeader = ByteBuffer.wrap(header); //convenience wrapper to write block sizes into a byte array
 
 			//the expected block sizes for sanity checking of the incoming blocks
 			final int[] blockSize = currentResLevel.blockDimensions.stream().mapToInt(i->i).toArray();
@@ -156,66 +170,46 @@ public class ImagePlusTransferrer extends ImagePlusDialogHandler {
 						final int ex = shortedBlockFlag[0] ? shortedBlockSize[0] : blockSize[0];
 						final int ey = shortedBlockFlag[1] ? shortedBlockSize[1] : blockSize[1];
 						final int ez = shortedBlockFlag[2] ? shortedBlockSize[2] : blockSize[2];
+						final int blockLength = ex*ey*ez * th.nativeAndRealType.getBitsPerPixel()/8;
 
 						myLogger.info("block at ["+x+","+y+","+z+"] px");
 						myLogger.info(" +- shorted block in x,y,z: "
 								+ shortedBlockFlag[0]+","+shortedBlockFlag[1]+","+shortedBlockFlag[2]);
-						myLogger.info(" +- I  expect  size: "+ex+" x "+ey+" x "+ez);
-
-						//retrieve the block header (which contains block size)
-						if (dataSrc.read(header,0,12) != 12)
-							throw new IOException("Failed reading full block header");
-						final ByteBuffer bb = ByteBuffer.wrap(header);
-						final int bx = bb.getInt();
-						final int by = bb.getInt();
-						final int bz = bb.getInt();
-						final int blockLength = bx*by*bz * th.nativeAndRealType.getBitsPerPixel()/8;
-
-						myLogger.info(" +- block says size: "+bx+" x "+by+" x "+bz
+						myLogger.info(" +- I report size: "+ex+" x "+ey+" x "+ez
 								+ " -> "+blockLength+" Bytes");
 
-						//check if the incoming block size fits the currently expected block size
-						checkBlockSizeAndPassOrThrow(bx,ex,'x');
-						checkBlockSizeAndPassOrThrow(by,ey,'y');
-						checkBlockSizeAndPassOrThrow(bz,ez,'z');
+						//write the block header (which contains block size)
+						wrapperOfHeader.rewind();
+						wrapperOfHeader.putInt(ex);
+						wrapperOfHeader.putInt(ey);
+						wrapperOfHeader.putInt(ez);
+						try {
+							dataTgt.write(header, 0, 12);
+						} catch (IOException e) {
+							throw new IOException("Failed writing full block header",e);
+						}
 
-						//make sure the buffer can accommodate the incoming data
+						//make sure the buffer can accommodate the outgoing data
 						if (blockLength > pxData.length)
 							pxData = new byte[blockLength];
 
-						//(eventually) read the block fully
-						int readSoFar = 0;
-						while (readSoFar < blockLength) {
-							//wait for data in a semi-busy wait
-							int tries = 0;
-							if (dataSrc.available() == 0 && tries < 10) {
-								Thread.sleep(2000);
-								++tries;
-							}
-							if (dataSrc.available() == 0)
-								throw new IOException("Gave up waiting for block data after reading "
-										+ readSoFar+" Bytes");
-
-							readSoFar += dataSrc.read(pxData,readSoFar,blockLength-readSoFar);
-						}
-						myLogger.info(" +- read "+readSoFar+" Bytes");
-
-						//copy the just-stored block content into the image
-						th.blockIntoImgInterval(pxData, bx*by*bz, Views.interval(img,
+						//copy the current image block into the buffer
+						th.imgIntervalIntoBlock( Views.interval(img,
 								new long[]{x-minX,      y-minY,      z-minZ},
-								new long[]{x-minX+bx-1, y-minY+by-1, z-minZ+bz-1}));
-					}
+								new long[]{x-minX+ex-1, y-minY+ey-1, z-minZ+ez-1}),
+								ex*ey*ez, pxData);
 
-/*
-			outDatasetImg = new DefaultDataset(this.getContext(),
-					new ImgPlus<T>(img,"Retrieved image at "+timepoints+","+channels+","+angles) );
-			myLogger.info("Created image: \""+outDatasetImg.getName()+"\"");
-*/
+						//(eventually) write the buffer (aka block) fully
+						dataTgt.write(pxData,0,blockLength);
+						//dataTgt.flush(); leave this decision on the subsystems...
+						myLogger.info(" +- wrote "+blockLength+" Bytes");
+					}
+			connection.getInputStream();
 
 		} catch (NoSuchElementException e) {
 			myLogger.error("Unrecognized voxel type: " + e.getMessage());
 			this.cancel("Unrecognized voxel type: " + e.getMessage());
-		} catch (IOException | InterruptedException | IllegalStateException e) {
+		} catch (IOException | IllegalStateException e) {
 			myLogger.error("Problem accessing the dataset: "+e.getMessage());
 			this.cancel("Problem accessing the dataset: "+e.getMessage());
 		}
