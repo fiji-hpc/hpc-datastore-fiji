bf5b70f45c6d967e2eec8652f33d947580a32967
Jan Kožusznik <jan@kozusznik.cz>
2022-05-04 11:04:30 +0200

    feat: Add File->Import->From HPC-Datastore... and File->Export->Into HPC-Datastore...
    Resolves: #1

---
+++ src/main/java/cz/it4i/fiji/legacy/FileExport.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * IT4Innovations - National Supercomputing Center
+ * Copyright (c) 2017 - 2022 All Right Reserved, https://www.it4i.cz
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this project.
+ ******************************************************************************/
+package cz.it4i.fiji.legacy;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, headless = true,
+	menuPath = "File>Export>Into HPC-Datastore...")
+public class FileExport extends WriteFullImage {
+
+
+}
